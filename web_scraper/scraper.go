81b53d58426aac5c81ec775228976bb1745cf9ee
emirrcaglar <emirrcaglar@gmail.com>
2025-07-06 23:11:51 +0300

    some progress on scrape

--- web_scraper/scraper.go
+++ web_scraper/scraper.go
@@ -2,26 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
-func requestHandler(url string) (resp *http.Response) {
+func requestHandler(url string) (resp *http.Response, body []byte) {
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		panic(err)
+		return
 	}
-	return resp
+	body, _ = io.ReadAll(resp.Body)
+
+	return resp, body
+}
+
+func htmlParser(resp *http.Response) (doc *html.Node, err error) {
+	doc, err = html.Parse(resp.Body)
+	if err != nil {
+		return
+	}
+
+	return doc, nil
 }
 
 func main() {
 	//... scraping logic
 
-	resp := requestHandler("https://scrape-me.dreamsofcode.io/")
+	_, body := requestHandler("https://scrape-me.dreamsofcode.io/")
 
-	fmt.Println(resp)
+	fmt.Println(string(body))
 }
