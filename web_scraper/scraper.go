914a4c79f720e067cf0fc8d4a3f0239fce8c2a34
emirrcaglar <emirrcaglar@gmail.com>
2025-07-07 09:42:36 +0300

    finds anchor tags

--- web_scraper/scraper.go
+++ web_scraper/scraper.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -23,19 +25,72 @@ func requestHandler(url string) (resp *http.Response, body []byte) {
 	return resp, body
 }
 
-func htmlParser(resp *http.Response) (doc *html.Node, err error) {
-	doc, err = html.Parse(resp.Body)
+func htmlParser(body []byte) (*html.Node, error) {
+	doc, err := html.Parse(bytes.NewReader(body))
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	return doc, nil
 }
 
-func main() {
-	//... scraping logic
+func printNode(n *html.Node, depth int) {
+	indent := strings.Repeat("  ", depth)
+	fmt.Printf("%sType: %d", indent, n.Type)
 
+	if n.Type == html.ElementNode {
+		fmt.Printf(", Tag: %s", n.Data)
+	} else if n.Type == html.TextNode {
+		fmt.Printf(", Text: %q", strings.TrimSpace(n.Data))
+	}
+
+	if len(n.Attr) > 0 {
+		fmt.Printf(", Attr: %v", n.Attr)
+	}
+	fmt.Println()
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		printNode(c, depth+1)
+	}
+}
+
+func findElements(n *html.Node, tag string) []*html.Node {
+	var nodes []*html.Node
+	if n.Type == html.ElementNode && n.Data == tag {
+		nodes = append(nodes, n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		nodes = append(nodes, findElements(c, tag)...)
+	}
+	return nodes
+}
+
+func main() {
 	_, body := requestHandler("https://scrape-me.dreamsofcode.io/")
 
-	fmt.Println(string(body))
+	doc, err := htmlParser(body)
+
+	if err != nil {
+		fmt.Println("Error parsing html", err)
+		return
+	}
+
+	anchorTags := findElements(doc, "a")
+
+	for i, anchor := range anchorTags {
+		fmt.Printf("Anchor #%d:\n", i+1)
+
+		for _, attr := range anchor.Attr {
+			if attr.Key == "href" {
+				fmt.Printf("	Link: %s\n", attr.Val)
+			}
+		}
+
+		for child := anchor.FirstChild; child != nil; child = child.NextSibling {
+			if child.Type == html.TextNode {
+				fmt.Printf("  Text: %q\n", strings.TrimSpace(child.Data))
+			}
+		}
+		fmt.Println()
+	}
 }
